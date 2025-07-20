fd44b6837825d074a43d80f2d0e10a714af4de6d
Alex260294 <evesby19@gmail.com>
2025-07-20 14:09:07 +0300

    init

---
+++ bot.py
@@ -0,0 +1,17 @@
+import os
+from aiogram import Bot, Dispatcher, types, executor
+from dotenv import load_dotenv
+
+# Читаем токен из .env
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def cmd_start(message: types.Message):
+    await message.answer("Привет, тренируемся!")
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
